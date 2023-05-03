bc7bc7fcc329b3c72e101f170c2e14957c188ded
tarttaros <jaosoriop_2@uqvirtual.edu.co>
2023-05-02 21:06:25 -0500

    test deploy

--- persistencia/src/main/java/co/edu/uniquindio/proyecto/repositorios/ProductCategoryRepo.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uniquindio.proyecto.repositorios;
-
-import co.edu.uniquindio.proyecto.entidades.ProductCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductCategoryRepo extends JpaRepository<ProductCategory,Integer> {
-}
