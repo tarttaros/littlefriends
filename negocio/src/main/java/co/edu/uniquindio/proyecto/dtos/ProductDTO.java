9c2636dd90218ba274ff75e1359a17fdf8f52531
tarttaros <jaosoriop_2@uqvirtual.edu.co>
2023-05-22 20:23:19 -0500

    correccion de smells

--- negocio/src/main/java/co/edu/uniquindio/proyecto/dtos/ProductDTO.java
+++ negocio/src/main/java/co/edu/uniquindio/proyecto/dtos/ProductDTO.java
@@ -16,22 +16,4 @@ public class ProductDTO implements Serializable {
     private Integer quantityProduct;
     private double price;
 
-    /*List<String> urlPhotos;
-
-    List<String> categoryList;
-
-    public void setUrlPhotos(List<ProductImage> productImages){
-        this.urlPhotos = new ArrayList<>();
-        productImages.forEach(productImage -> {
-            this.urlPhotos.add(productImage.getUrl());
-        });
-    }
-
-    public void setCategoryList(List<Category> categories){
-        this.categoryList = new ArrayList<>();
-        categories.forEach(category -> {
-            this.categoryList.add(category.getDescription());
-        });
-    }
-*/
 }
