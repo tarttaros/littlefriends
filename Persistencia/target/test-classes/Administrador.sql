b6f5bc94de46e14aa003485ac4ad5e8d2fca8dcd
Jhonatan <jcuribep@uqvirtual.edu.co>
2023-05-22 22:00:30 -0500

    algo

---
+++ Persistencia/target/test-classes/Administrador.sql
@@ -0,0 +1,3 @@
+insert into admin (id_admin,email,password) values (1,"pablo@gmail.com","147");
+insert into admin (id_admin,email,password) values (2,"andrea@gmail.com","123");
+insert into admin (id_admin,email,password) values (3,"luisa@gmail.com","321");
