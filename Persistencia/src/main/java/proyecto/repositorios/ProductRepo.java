6c4ebe9da69893d870ffdb22fb56172a161ccd26
Jhonatan <jcuribep@uqvirtual.edu.co>
2023-05-22 22:58:11 -0500

    algo5

--- Persistencia/src/main/java/proyecto/repositorios/ProductRepo.java
+++ Persistencia/src/main/java/proyecto/repositorios/ProductRepo.java
@@ -15,8 +15,7 @@ public interface ProductRepo extends JpaRepository<Product,Integer> {
     @Query("select p.nameProduct as name,sum(pd.units) as units,sum(pd.price*pd.units) as totalPrice,p.quantityProduct as available from Product p join PurchaseDetail pd on p.id=pd.product.id order by p.nameProduct")
     List<Object[]> getReportProduct();
 
-    //@Query("select new co.edu.uniquindio.proyecto.dto.ReportProductDto(name,units,totalPrice,available) from (")
-    //List<ReportProductDto> ;
+
 
     @Query("select pi from ProductImage pi where pi.product.id=:id")
     List<ProductImage> getProductImageByIdProduct(Integer id);
