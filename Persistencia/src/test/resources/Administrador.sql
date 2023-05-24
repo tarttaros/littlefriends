44fdb283455382da6a1781bd6de122d59f997518
tarttaros <jaosoriop_2@uqvirtual.edu.co>
2023-05-24 02:53:04 -0500

    configuracion final1

--- Persistencia/src/test/resources/Administrador.sql
+++
@@ -1,3 +0,0 @@
-insert into admin (id_admin,email,password) values (1,"pablo@gmail.com","147");
-insert into admin (id_admin,email,password) values (2,"andrea@gmail.com","123");
-insert into admin (id_admin,email,password) values (3,"luisa@gmail.com","321");
--- Persistencia/src/test/resources/Purchases.sql
+++
@@ -1,9 +0,0 @@
-
-insert into user(id_user,date_creation,email,identification,name,password,phone_number,photo_url,sex) values (1,"2023/5/15","angela@gmail.com","115170980","angela marin","55500","318814414","imagen1",2);
-insert into user(id_user,date_creation,email,identification,name,password,phone_number,photo_url,sex) values (2,"2023/1/12","pedro@gmail.com","160890980","pedro antonio","55500","318814414","imagen1",1);
-insert into user(id_user,date_creation,email,identification,name,password,phone_number,photo_url,sex) values (3,"2023/5/15","jesica@gmail.com","119168980","jesica robledo","18962400","318811114","imagen1",2);
-
-insert into purchase (id_purchase,date_purchase,payment_method,id_user) values (1,"2020/5/12","debito",1);
-insert into purchase (id_purchase,date_purchase,payment_method,id_user) values (2,"2023/6/10","credito",3);
-insert into purchase (id_purchase,date_purchase,payment_method,id_user) values (3,"2021/7/19","debito",2);
-
