e9df9a022d0c84ffe6d930b07c8c19760defc792
tarttaros <jaosoriop_2@uqvirtual.edu.co>
2023-05-23 19:55:32 -0500

    configuracion final1

--- Persistencia/target/test-classes/ProductImages.sql
+++
@@ -1,3 +0,0 @@
-insert into product_image(id_product_image,url) values (1,"imagen1");
-insert into product_image(id_product_image,url) values (2,"imagen2");
-insert into product_image(id_product_image,url) values (3,"imagen3");
--- Persistencia/target/test-classes/Veterinaries.sql
+++
@@ -1,2 +0,0 @@
-insert into veterinary (id_veterinary,email,identification,name,password,phone_number,photo_url,sex,num_license) values (1,"maria@gmail.com","1915841052","maria uribe","202041","3188545796","imagen2",2,"1414541414");
-insert into veterinary (id_veterinary,email,identification,name,password,phone_number,photo_url,sex,num_license) values (2,"carlos@gmail.com","1816841781","carlos velez","45101","313874414","imagen3",1,"4488441414");
