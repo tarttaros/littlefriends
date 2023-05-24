89831315c94dd7d65b1b0f6e0730f9f5dc14e96b
tarttaros <jaosoriop_2@uqvirtual.edu.co>
2023-05-24 03:57:41 -0500

    configuracion final1

--- negocio/src/main/java/co/edu/uniquindio/proyecto/rest/VeterinaryRestController.java
+++ negocio/src/main/java/co/edu/uniquindio/proyecto/rest/VeterinaryRestController.java
@@ -27,12 +27,12 @@ public class VeterinaryRestController {
     private TokenService tokenService;
 
     @PostMapping("/vet")
-    public ResponseEntity<Veterinary> saveVeterinary(@RequestBody VeterinaryDTO veterinary) throws  Exception{
+    public ResponseEntity saveVeterinary(@RequestBody VeterinaryDTO veterinary) throws  Exception{
         try {
             Veterinary v = veterinaryService.createVeterinary(new Veterinary(veterinary.getName(),veterinary.getPhoneNumber(),veterinary.getIdentification(),veterinary.getEmail(),"",veterinary.getSex(), Hash.factory().toSha1(veterinary.getPassword()), LocalDateTime.now(),veterinary.getNumLicense()));
             return ResponseEntity.ok(v);
         }catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+            return new ResponseEntity<String>(HttpStatus.EXPECTATION_FAILED);
         }
     }
 
